eba5b17116086e942fcc2f444adc1a55d36106eb
Ken Kousen <ken.kousen@kousenit.com>
2018-06-19 10:55:18 -0400

    Add assertion demos with suppliers

--- src/test/java/com/oreilly/SimpleTest.java
+++ src/test/java/com/oreilly/SimpleTest.java
@@ -1,5 +1,6 @@
 package com.oreilly;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,5 +9,12 @@ public class SimpleTest {
     @Test
     void firstTest() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test @Disabled("Demo of string failure message")
+    void failureWithAStringMessage() {
+        int x = 2;
+        int y = 3;
+        assertEquals(2, x + y, "Sum should have been 2");
     }
 }
