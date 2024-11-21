3da3cd2d0dd743e75d32618165b50964bbde859d
Ken Kousen <ken.kousen@kousenit.com>
2024-11-21 10:04:28 -0500

    Update data source config, Gradle, and AssertJDB tests
    Added in-memory H2 database configuration in application.yml. Updated Gradle to version 8.11.1 and synchronized dependency versions in libs.versions.toml. Revised AssertJDB tests to use AssertDbConnectionFactory, removing dependency on Spring's DataSource.

--- spring/src/test/java/com/oreilly/dao/AssertJDBTests.java
+++ spring/src/test/java/com/oreilly/dao/AssertJDBTests.java
@@ -1,55 +1,72 @@
 package com.oreilly.dao;
 
+import org.assertj.db.type.AssertDbConnection;
+import org.assertj.db.type.AssertDbConnectionFactory;
 import org.assertj.db.type.Table;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.sql.DataSource;
 
 import static org.assertj.db.api.Assertions.assertThat;
 
 @SpringBootTest
 public class AssertJDBTests {
-    @Autowired
-    private DataSource dataSource;
+    private Table table;
+
+    @BeforeEach
+    void setUp() {
+        AssertDbConnection connection = AssertDbConnectionFactory.of(
+                        "jdbc:h2:mem:testdb", "sa", "sa")
+                .create();
+        table = connection.table("officers")
+                .build();
+    }
 
     @Test
     void checkLastNameColumnValues() {
-        Table table = new Table(dataSource, "officers");
         assertThat(table).column("last_name")
-                .value().isEqualTo("Kirk")
-                .value().isEqualTo("Picard")
-                .value().isEqualTo("Sisko")
-                .value().isEqualTo("Janeway")
-                .value().isEqualTo("Archer");
+                .value()
+                .isEqualTo("Kirk")
+                .value()
+                .isEqualTo("Picard")
+                .value()
+                .isEqualTo("Sisko")
+                .value()
+                .isEqualTo("Janeway")
+                .value()
+                .isEqualTo("Archer");
     }
 
     @Test
     void checkLastNames() {
-        Table table = new Table(dataSource, "officers");
         assertThat(table).column("last_name")
                 .hasValues("Kirk", "Picard", "Sisko", "Janeway", "Archer");
     }
 
     @Test
     void checkFirstTwoRows() {
-        Table table = new Table(dataSource, "officers");
         assertThat(table).row(0)
-                .value().isEqualTo(1)
-                .value().isEqualTo("CAPTAIN")
-                .value().isEqualTo("James")
-                .value().isEqualTo("Kirk");
+                .value()
+                .isEqualTo(1)
+                .value()
+                .isEqualTo("CAPTAIN")
+                .value()
+                .isEqualTo("James")
+                .value()
+                .isEqualTo("Kirk");
         assertThat(table).row(1)
-                .value().isEqualTo(2)
-                .value().isEqualTo("CAPTAIN")
-                .value().isEqualTo("Jean-Luc")
-                .value().isEqualTo("Picard");
+                .value()
+                .isEqualTo(2)
+                .value()
+                .isEqualTo("CAPTAIN")
+                .value()
+                .isEqualTo("Jean-Luc")
+                .value()
+                .isEqualTo("Picard");
     }
 
     @Test
     void tableSizeAssertions() {
-        Table table = new Table(dataSource, "officers");
         assertThat(table).hasNumberOfColumns(4);
         assertThat(table).hasNumberOfRows(5);
     }
